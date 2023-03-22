f69c4bdb814110af7a7e4cfe90be53729ecc658c
Sharvan TG <sharvan@plumhq.com>
2023-03-22 14:47:28 +0530

    Made some changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 //this is comment to check if the commit works
+
+const industries = new Map();
+$('.industry-object').each(function () {
+  const s = $(this).children('div.industry-name'); //industry name
+  const f = s[0].innerText;
+  const v = $(this).children('div.eligibility'); //eligibility status
+  const e = v[0].innerText;
+  //adding the values into a map
+  industries.set(f, e);
+  $('#eligible-industry').append(`<option value="` + f + `">` + f + `</option>`);
+});
