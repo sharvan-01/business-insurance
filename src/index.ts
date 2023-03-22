553c244bccff26893defa0872f4bf429d4e7b42c
Sharvan TG <109077537+sharvan-01@users.noreply.github.com>
2023-03-22 14:16:05 +0530

    Added the industry select option

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 //this is comment to check if the commit works
+var industryName;
+
+const industries = new Map();
+$('.industry-name-item').each(function () {
+    const s = $(this).children('div.industry-name'); //industry name
+    industryName  = s[0].innerText;
+    //adding the values into a map
+    industries.set(f, e);
+    $('#Industry').append(`<option value="` + f + `">` + f + `</option>`);
+});
+
+
+
+
